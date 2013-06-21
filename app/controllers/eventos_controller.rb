c9207bc6dee1e3eac3d7a251cf38ec965fbb730f
samartin <samuel.martinm@gmail.com>
2013-06-21 17:57:18 +0200

    Añadido modificar cuando eres profesor

--- app/controllers/eventos_controller.rb
+++ app/controllers/eventos_controller.rb
@@ -1,5 +1,11 @@
 class EventosController < ApplicationController
   def show
     @evento = Evento.find(params[:id])
+    meses = Mes.all
+    meses.each do |mes|
+    	if mes.nmes==@evento.fecha.month.to_s
+    		@mes=mes
+    	end
+    end
   end
 end
