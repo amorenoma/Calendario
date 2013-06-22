58ec9a29c866c85d98828853ccafc3c6dd8f7f32
samartin <samuel.martinm@gmail.com>
2013-06-22 18:56:59 +0200

    Añadido panel de administracion de comentarios en modo profesor con opciones de borrado y enlaces a los eventos

--- app/controllers/comentarios_controller.rb
+++ app/controllers/comentarios_controller.rb
@@ -12,4 +12,16 @@ class ComentariosController < ApplicationController
   	redirect_to evento_path(session[:evento])
   end
 
+  def index
+    #este es un modo de ordenar (en el show.html.haml el otro):
+    @comentarios = Comentario.find(:all)
+  end
+
+  def destroy
+    @comentario = Comentario.find(params[:id])
+    @comentario.destroy
+    flash[:notice]= "El comentario ha sido borrado"
+    redirect_to comentarios_path
+  end
+
 end
