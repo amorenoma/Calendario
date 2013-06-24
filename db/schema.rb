43fb5bbb2c4ede263553ecf5a7d41a9006fd9481
aniiichiii <ranaranitaranon@hotmail.com>
2013-06-24 12:11:22 +0200

    fecha en comentarios

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,7 @@ ActiveRecord::Schema.define(:version => 20130621181944) do
     t.string   "comentario"
     t.string   "evento"
     t.string   "usuario"
+    t.datetime "fecha"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
