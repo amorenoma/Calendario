5d3c79273f210be45cdd9cf99e3d264a4e7ba678
amorenoma <wineblado@gmail.com>
2013-06-24 03:07:28 -0700

    mas asignaturas

--- app/models/evento.rb
+++ app/models/evento.rb
@@ -2,6 +2,6 @@ class Evento < ActiveRecord::Base
   attr_accessible :fecha, :asignatura, :tipo, :aula, :dificultad
 
   def self.all_asignaturas
-    %w(ISI TC IST SAT DAT)
+    %w(ISI TC IST SAT DAT BIO PDAC AC SSOO PST Circuitos Calculo FDP CC IO)
   end
 end
