3cda574b9f97e8c56149b8b499f93dce4fae91bd
rodri328 <digi121@hotmail.com>
2013-06-26 06:34:40 -0700

    pestaña para elegir rol

--- spec/controllers/usuarios_controller_spec.rb
+++ spec/controllers/usuarios_controller_spec.rb
@@ -10,5 +10,18 @@ describe UsuariosController do
       response.should redirect_to(homepage_index_path)
       flash[:notice].should == "Flexo se ha registrado"
     end
+    it 'modificar un usuario' do
+      usr = mock('evt1')
+      Usuario.should_receive(:find).with('1').and_return(usr)
+      get :edit, {:id => 1}
+      response.should render_template("edit")
+    end
+    it 'borrar un usuario' do
+      usr = mock('evt1')
+      Usuario.should_receive(:find).with('1').and_return(usr)
+      usr.should_receive(:destroy).and_return(true)
+      post :destroy, {:id => '1'}
+      flash[:notice].should == "Usuario borrado"
+    end
   end
 end
